764e4f92bcef553f2d8acf3a3f3e4ee0cf3a64c6
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-02-13 10:36:04 -0800

    fixed link href

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <body>
     <h1>Sukhvir Singh</h1>
     <p>This website is running on a Digital Ocean droplet with a LAMP architecture (Linux, Apache, MySQL, PHP). </p>
-    <p>You can access phpmyadmin at <a href="sukhvir-singh.com/phpmyadmin">sukhvir-singh.com/phpmyadmin</a></p>
+    <p>You can access phpmyadmin at <a href="/phpmyadmin">sukhvir-singh.com/phpmyadmin</a></p>
     <p>This website has also been secured with SSL certificates.</p>
     <?php
         echo("testing php");
