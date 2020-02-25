7fe05beb308481acac732304d36428fa9f6eb64b
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-02-25 13:20:37 -0800

    added other pages

--- index.php
+++ index.php
@@ -7,12 +7,19 @@
     <title>Sukhvir Singh's Website</title>
 </head>
 <body>
-    <h1>Sukhvir Singh - CMPE 272</h1>
+    <h1>Sukhvir Singh</h1>
+    <div class="topnav">
+        <a href="#">Home</a>
+        <a href='./products'>Products</a>
+        <a href="./news">News</a>
+        <a href="./contacts">Contact</a>
+        <a href="./about">About</a>
+    </div>
     <p>This website is running on a Digital Ocean droplet with a LAMP architecture (Linux, Apache, MySQL, PHP). </p>
     <p>You can access phpmyadmin at <a href="/phpmyadmin">sukhvir-singh.com/phpmyadmin</a></p>
     <p>This website has also been secured with SSL certificates.</p>
-    <?php
+    <!-- <?php
         echo("This line is printed using php to show that php works");
-    ?>
+    ?> -->
 </body>
 </html>
