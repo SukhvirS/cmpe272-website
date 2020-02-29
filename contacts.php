7ef6b21650f2ae28faa2c4ad262e6ebc5cb6858e
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-02-29 10:01:32 -0800

    changed navbar style

--- contacts.php
+++ contacts.php
@@ -11,13 +11,13 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#"><h1>Sukhvir Singh</h1></a>
+      <h1 style="padding-right: 40px;">Sukhvir Singh</h1>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
       
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
+          <ul class="nav nav-pills">
             <li class="nav-item">
               <a class="nav-link" href="index.html">Home</a>
             </li>
@@ -27,8 +27,8 @@
             <li class="nav-item">
                 <a class="nav-link" href="news.html">News</a>
             </li>
-            <li class="nav-item active">
-              <a class="nav-link" href="contacts.html">Contacts<span class="sr-only">(current)</span></a>
+            <li class="nav-item">
+              <a class="nav-link active" href="./contacts.php">Contacts</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="about.html">About</a>
