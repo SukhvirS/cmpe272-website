97145fd8a9626c139e472f2c427af96f0c56028d
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-03-03 16:12:43 -0800

    changed contacts list style

--- contacts.php
+++ contacts.php
@@ -39,8 +39,8 @@
     </nav>
     <div>
         <h2>Contacts</h2>
-        <div style="float:">
-          <ul class="list-group list-group-flush">
+        <div>
+          <ul class="list-group">
             <?php
               $file = file('all_contacts.txt');
               for($x = 0; $x < count($file); $x++){
@@ -52,8 +52,6 @@
             ?>
           </ul>
         </div>
-
-
     </div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
