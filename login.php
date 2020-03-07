8eb6ea2eac7b012ed25cbea9d5597b145b84a8da
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-03-07 11:06:24 -0800

    testing

--- login.php
+++ login.php
@@ -95,11 +95,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Cinzel|Comfortaa|Montserrat|Noto+Serif+TC|Playfair+Display|Questrial|Simonetta|Work+Sans&display=swap" rel="stylesheet"> -->
+    <link rel="stylesheet" href="main.css">
+    
 </head>
 <body>
     <div class="wrapper">
