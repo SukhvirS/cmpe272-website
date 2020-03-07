98cba1c78e4a06f78531ace138eea5eb498d4e20
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-03-07 11:18:23 -0800

    admin login works now

--- login.php
+++ login.php
@@ -106,7 +106,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <br>
         <br>
         <h2>Login</h2>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <form class="form-signin" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                 <label>Username</label>
                 <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
