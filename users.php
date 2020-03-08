a9a426e517ace7c8b57247e4de8ffd917b053aa6
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-03-07 19:00:46 -0800

    added placeholder buttons

--- users.php
+++ users.php
@@ -11,96 +11,138 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Comfortaa&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="main.css">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+      <link href="https://fonts.googleapis.com/css?family=Comfortaa&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="main.css">
 
-    <title>Users</title>
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
-      <h1>The Phone Company</h1>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="nav navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="products.php">Products</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="news.php">News</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="about.php">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" href="users.php">Users</a>
-            </li>
-          </ul>
-        </div>
+      <title>Users</title>
+  </head>
+  <body>
+      <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+        <h1>The Phone Company</h1>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="nav navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="products.php">Products</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="news.php">News</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="users.php">Users</a>
+              </li>
+            </ul>
+          </div>
 
-        <?php
-          session_start();
-
-          if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-            echo('
-              <form method="post"> 
-                <input type="submit" name="logout-button"
-                        class="btn btn-dark" value="Logout" /> 
-              </form> 
-                  
-            ');
-          }
-
-          function logout(){
-            // Initialize the session
+          <?php
             session_start();
-            
-            // Unset all of the session variables
-            $_SESSION = array();
-            
-            // Destroy the session.
-            session_destroy();
-            
-            // Redirect to homepage
-            header("location: index.php");
-            exit;
-          }
 
-          if(array_key_exists('logout-button', $_POST)) { 
-            logout(); 
-          } 
+            if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+              echo('
+                <form method="post"> 
+                  <input type="submit" name="logout-button"
+                          class="btn btn-dark" value="Logout" /> 
+                </form> 
+                    
+              ');
+            }
 
-        ?>
-    </nav>
-    <div>
-        <h2>Users</h2>
-        <div>
-          <ul class="list-group">
-            <?php
-              $file = file('all_contacts.txt');
-              for($x = 0; $x < count($file); $x++){
-                echo('<li class="list-group-item">');
-                echo(($x+1).'. ');
-                echo($file[$x]);
-                echo('</li>');
-              }
-            ?>
-          </ul>
+            function logout(){
+              // Initialize the session
+              session_start();
+              
+              // Unset all of the session variables
+              $_SESSION = array();
+              
+              // Destroy the session.
+              session_destroy();
+              
+              // Redirect to homepage
+              header("location: index.php");
+              exit;
+            }
+
+            if(array_key_exists('logout-button', $_POST)) { 
+              logout(); 
+            } 
+
+          ?>
+      </nav>
+      <div style="margin: 10px 10px 0 10px">
+        <h2 style="float: left; margin: 0">Users</h2>
+        <div style="float: right">
+          <span>
+            <div style="width:400px">
+
+              <!-- Search button -->
+              <div style="width: 250px; float: left" class="input-group md-form form-sm form-2 pl-0">
+                <input class="form-control my-0 py-1 red-border" type="text" placeholder="Search" aria-label="Search">
+                <div class="input-group-append">
+                  <span class="input-group-text red lighten-3" id="basic-text1"><i class="fa fa-search text-grey"
+                      aria-hidden="true"></i></span>
+                </div>
+              </div>
+
+              <!-- Add user button -->
+              <button style="float: right" class="btn btn-outline-primary" data-toggle="modal" data-target="#addUserModal">+ Add User</button>
+              
+              <!-- Add user modal -->
+              <div class="modal fade modal-lg" id="addUserModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                      ...
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </span>
         </div>
+      </div>
+
+
+      <div>
+        <ul class="list-group">
+          <?php
+            $file = file('all_contacts.txt');
+            for($x = 0; $x < count($file); $x++){
+              echo('<li class="list-group-item">');
+              echo(($x+1).'. ');
+              echo($file[$x]);
+              echo('</li>');
+            }
+          ?>
+        </ul>
+      </div>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-</body>
+  </body>
 </html>
