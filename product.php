26d59d16bd48a868ed96b62f7d99ffcf4be355c1
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-03-10 12:16:04 -0700

    testing

--- product.php
+++ product.php
@@ -46,8 +46,7 @@
                 <form method="post"> 
                     <input type="submit" name="logout-button"
                             class="btn btn-dark" value="Logout" /> 
-                </form> 
-                    
+                </form>
                 ');
             }
 
@@ -94,10 +93,10 @@
               return currentParam[1];
             }
           }
-          console.log(document.getElementById('product-name'));
 
           document.getElementById('product-name').innerHTML = parseUrlParameter('name');
         }
+        console.log(document.getElementById('product-name'));
 
     </script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
