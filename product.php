404016f562bc8cace1b5436748820d97da14a24a
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-03-10 13:32:42 -0700

    testing

--- product.php
+++ product.php
@@ -79,7 +79,9 @@
             $currentUrl = 'https://';
             $currentUrl .= $_SERVER['HTTP_HOST'];
             $currentUrl .= $_SERVER['REQUEST_URI'];
-            $index = $currentUrl[strpos($currentUrl, 'index') + 6];
+            $indexInURL = $currentUrl[strpos($currentUrl, 'index') + 6];
+            $index = substr($currentUrl, $indexInURL);
+            echo($index);
             echo('
             <img src="'.$products[$index+1].'"></img>"
             ');
