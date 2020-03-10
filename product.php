2277e301c0f6f3925103d5048cc6a5376f5b2a28
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-03-10 12:10:10 -0700

    testing

--- product.php
+++ product.php
@@ -51,24 +51,25 @@
             ');
           }
 
-          function logout(){
-            // Initialize the session
-            session_start();
-            
-            // Unset all of the session variables
-            $_SESSION = array();
-            
-            // Destroy the session.
-            session_destroy();
-            
-            // Redirect to homepage
-            header("location: index.php");
-            exit;
-          }
+            function logout(){
+                // Initialize the session
+                session_start();
 
-          if(array_key_exists('logout-button', $_POST)) { 
-            logout(); 
-          } 
+                // Unset all of the session variables
+                $_SESSION = array();
+
+                // Destroy the session.
+                session_destroy();
+
+                // Redirect to homepage
+                header("location: index.php");
+                exit;
+                }
+
+                if(array_key_exists('logout-button', $_POST)) { 
+                logout(); 
+                } 
+            }
 
         ?>
     </nav>
