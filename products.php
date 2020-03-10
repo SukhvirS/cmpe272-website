feca2e79fd7ccce2a27b68d828d910432915af20
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-03-10 13:53:43 -0700

    testing

--- products.php
+++ products.php
@@ -83,10 +83,10 @@
             $price = $products[$x+2];
             echo('
               <a href="product.php?name='.str_replace(' ','-',$name).'&index='.$x.'">
-                <div class="my-card" style="width: 18rem; margin:8px 6px">
+                <div class="box" >
                   <img src="'.$products[$x+1].'" alt="..." height="200px" style="display: block; margin: 20px auto 0 auto">
-                  <div >
-                    <h5  style="color: black">'.$products[$x].'</h5>
+                  <div class="card-body">
+                    <h5 class="card-title" style="color: black">'.$products[$x].'</h5>
                     <a href="#" class="btn btn-primary">'.$products[$x+2].'</a>
                   </div>
                 </div>
