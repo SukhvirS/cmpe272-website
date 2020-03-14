6fb53bb4216199f2495f6eb1a714fc585f19cd50
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-03-14 16:31:50 -0700

    testing

--- users.php
+++ users.php
@@ -225,7 +225,7 @@
                 if($_SERVER["REQUEST_METHOD"] == "POST"){
                   $sql = "SELECT * FROM customers WHERE firstName LIKE '%$searchInput%' OR lastName LIKE '%$searchInput%' OR email LIKE '%$searchInput%' OR homePhone LIKE '%$searchInput%' OR cellPhone LIKE '%$searchInput%'";
                   if($searchInput != ''){
-                    echo("<h2>".$searchInput."</h2>");
+                    echo("<h2>Showing results for: ".$searchInput."</h2>");
                   }
 
                   if($result = mysqli_query($link, $sql)){
