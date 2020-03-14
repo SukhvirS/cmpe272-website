affe0759ef673126b75cab542edf8370cdf0e192
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-03-14 13:39:45 -0700

    testing

--- product.php
+++ product.php
@@ -82,22 +82,21 @@
 
             echo('
             <p>'.$row['name'].'</p>
-            <img src="'.$row['img1Url'].'"></img>
             <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
               <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
               </ol>
-              <div class="carousel-inner" style="height: 400px; width: 200px">
+              <div class="carousel-inner" >
                 <div class="carousel-item active">
-                  <img class="d-block w-100" src="'.$row['img1Url'].'" alt="First slide" >
+                  <img class="d-block w-100" src="'.$row['img1Url'].'" alt="First slide" style="height:400px; max-height: 400px; width: auto;">
                 </div>
                 <div class="carousel-item">
-                  <img class="d-block w-100" src="'.$row['img2Url'].'" alt="Second slide" >
+                  <img class="d-block w-100" src="'.$row['img2Url'].'" alt="Second slide" style="height:400px; max-height: 400px; width: auto;">
                 </div>
                 <div class="carousel-item">
-                  <img class="d-block w-100" src="'.$row['img3Url'].'" alt="Third slide" >
+                  <img class="d-block w-100" src="'.$row['img3Url'].'" alt="Third slide" style="height:400px; max-height: 400px; width: auto;">
                 </div>
               </div>
               <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
