7f60d65785b6007ca43728369d1e6c014d023d14
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-03-14 16:19:43 -0700

    testing

--- users.php
+++ users.php
@@ -224,7 +224,7 @@
 
                 if($_SERVER["REQUEST_METHOD"] == "POST"){
                   $sql = "SELECT * FROM customers WHERE firstName LIKE '%$searchInput%' OR lastName LIKE '%$searchInput%' OR email LIKE '%$searchInput%' OR homePhone LIKE '%$searchInput%' OR cellPhone LIKE '%$searchInput%'";
-                  echo("<h2>".$searchInput."</h2>");
+                  echo("<h4>You searched for: ".$searchInput."</h2>");
 
                   if($result = mysqli_query($link, $sql)){
                     $rowCount = mysqli_num_rows($result);
