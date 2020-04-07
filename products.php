c36021094743c89a02cb9f486ce89b146247ebd7
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-04-06 23:13:23 -0700

    testing

--- products.php
+++ products.php
@@ -118,7 +118,7 @@
             if($result = mysqli_query($link, $sql)){
               while($row = mysqli_fetch_assoc($result)){
                 echo('
-                <a href="product.php?name='.str_replace(' ','-',$row["name"]).'&index='.$row["productID"].'" onclick="updateRecentlyViewed('.$row["productID"].')">
+                <a href="#" onclick="updateRecentlyViewed('.$row["productID"].')">
                   <div class="card" style="width:18rem; margin:8px 6px">
                     <img src="'.$row["img1Url"].'" alt="..." height="200px" style="display: block; margin: 20px auto 0 auto">
                     <div class="card-body">
