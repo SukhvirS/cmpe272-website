c872aff3f9485d8e68642534b45a0d0ba472237c
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-04-06 23:14:16 -0700

    testing

--- products.php
+++ products.php
@@ -26,7 +26,7 @@
           var items = JSON.parse(localStorage.getItem('mostRecent'));
           if(items == null){
             items = [x];
-            locaStorage.setItem('mostRecent', JSON.stringify(items));
+            localStorage.setItem('mostRecent', JSON.stringify(items));
           }
           else{
             if(items.length != 5){
@@ -118,7 +118,7 @@
             if($result = mysqli_query($link, $sql)){
               while($row = mysqli_fetch_assoc($result)){
                 echo('
-                <a href="#" onclick="updateRecentlyViewed('.$row["productID"].')">
+                <a href="product.php?name='.str_replace(' ','-',$row["name"]).'&index='.$row["productID"].'" onclick="updateRecentlyViewed('.$row["productID"].')">
                   <div class="card" style="width:18rem; margin:8px 6px">
                     <img src="'.$row["img1Url"].'" alt="..." height="200px" style="display: block; margin: 20px auto 0 auto">
                     <div class="card-body">
