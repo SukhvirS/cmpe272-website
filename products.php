76e2722c7d118d1780ac5e9248323c2d697da82b
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-04-06 20:48:50 -0700

    testing

--- products.php
+++ products.php
@@ -21,6 +21,12 @@
       }
     </style>
 
+      <script>
+        function updateRecentlyViewed(x){
+          console.log(x);
+        }
+      </script>
+
     <title>Products</title>
 </head>
 <body>
@@ -88,7 +94,7 @@
             if($result = mysqli_query($link, $sql)){
               while($row = mysqli_fetch_assoc($result)){
                 echo('
-                <a href="product.php?name='.str_replace(' ','-',$row["name"]).'&index='.$row["productID"].'">
+                <a href="product.php?name='.str_replace(' ','-',$row["name"]).'&index='.$row["productID"].' onclick=updateRecentlyViewed('.$row["productID"].')">
                   <div class="card" style="width:18rem; margin:8px 6px">
                     <img src="'.$row["img1Url"].'" alt="..." height="200px" style="display: block; margin: 20px auto 0 auto">
                     <div class="card-body">
@@ -106,8 +112,9 @@
       </div>
     </div> 
 
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
 </body>
 </html>
