351bcef60b4cbc01e08892a4f864cabdbcaebbc7
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-04-07 20:32:48 -0700

    testing

--- products.php
+++ products.php
@@ -22,28 +22,12 @@
     </style>
 
     <script>
-      // function sortByValue(obj)
-      // {
-      //   // convert object into array
-      //   var sortable=[];
-      //   for(var key in obj)
-      //     if(obj.hasOwnProperty(key))
-      //       sortable.push([key, obj[key]]); // each item is an array in format [key, value]
-        
-      //   // sort items by value
-      //   sortable.sort(function(a, b)
-      //   {
-      //     return a[1]-b[1]; // compare numbers
-      //   });
-      //   return sortable; // array in format [ [ key1, val1 ], [ key2, val2 ], ... ]
-      // }
-
       function compareSecondColumn(a, b) {
           if (a[1] === b[1]) {
               return 0;
           }
           else {
-              return (a[1] < b[1]) ? -1 : 1;
+              return (a[1] > b[1]) ? -1 : 1;
           }
       }
 
