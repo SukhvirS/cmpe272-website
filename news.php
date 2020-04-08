02be07b0708992ae202f9e4c3cec53712a34a491
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-04-08 13:11:42 -0700

    testing

--- news.php
+++ news.php
@@ -10,7 +10,7 @@
     <title>News</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark bg-transparent">
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
       <h1>The Phone Company</h1>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
