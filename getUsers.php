3a9170f8532a69a24e6e65ed778b8027794d11a4
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-04-19 15:18:20 -0700

    testing

---
+++ getUsers.php
@@ -0,0 +1,28 @@
+<?php
+    session_start();
+
+    if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+        header("location: login.php");
+        exit;
+    }
+
+    require_once('config.php');
+
+    $sql = "SELECT * FROM customers";
+    $result = '';
+
+    if($result = mysqli_query($link, $sql)){
+        while($row = mysqli_fetch_assoc($result)){
+            $result += $row['firstName'] + ',';
+            $result += $row['lastName'] + ',';
+            $result += $row['address'] + ',';
+            $result += $row['email'] + ',';
+            $result += $row['homePhone'] + ',';
+            $result += $row['cellPhone'] + ',';
+        }
+        mysqli_free_result($result);
+    }
+
+    mysqli_close($link);
+    echo($result);
+?>
