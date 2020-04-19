930fd9350ce5ec3a5676727e99501515f95cf914
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-04-19 12:09:20 -0700

    testing

--- allUsers.php
+++ allUsers.php
@@ -1,3 +1,20 @@
 <?php
-    echo("test")
+    $data = array(
+        "USERNAME" => "admin",
+        "PASSWORD" => "admin",
+
+    );
+
+    $ch = curl_init("http://cmpe272.nicolas-hanout.com/login.php");
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    echo $result;
+
 ?>
