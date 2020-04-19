73a5547a760ac19c666ab6fa0adcdb86c983bd14
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-04-19 15:25:12 -0700

    tetsing

--- getUsers.php
+++ getUsers.php
@@ -1,13 +1,13 @@
 <?php
-    session_start();
+    // session_start();
 
-    if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-        header("location: login.php");
-        exit;
-    }
-    else{
-        header("location: getUsers.php");
-    }
+    // if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    //     header("location: login.php");
+    //     exit;
+    // }
+    // else{
+    //     header("location: getUsers.php");
+    // }
 
     require_once('config.php');
 
