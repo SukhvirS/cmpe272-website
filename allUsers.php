c81fbad809fd16556e8489f5c00ed78f0e59079c
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-04-19 18:11:34 -0700

    testing

--- allUsers.php
+++ allUsers.php
@@ -106,17 +106,23 @@
           <tbody>
             <?php
                 $ar1 = explode("\n", $result);
+                echo($ar1[0]);
+                echo("<br>");
+                echo($ar1[0]);
+                echo("<br>");
+                echo($ar1[0]);
+                echo("<br>");
                 $ar2 = Array();
-                foreach($ar1 as $el1){
-                    echo("<tr>");
-                    echo("<tr>".$el1."</tr>");
-                    // $ar2 = explode(",", $el1);
-                    // foreach($ar2 as $el2){
-                    //     if($el2 != ''){
-                    //         echo("<td>".$el2."</td>");
-                    //     }
-                    // }
-                }
+                // foreach($ar1 as $el1){
+                //     echo("<tr>");
+                //     echo("<tr>".$el1."</tr>");
+                //     // $ar2 = explode(",", $el1);
+                //     // foreach($ar2 as $el2){
+                //     //     if($el2 != ''){
+                //     //         echo("<td>".$el2."</td>");
+                //     //     }
+                //     // }
+                // }
             ?>
             
           </tbody>
