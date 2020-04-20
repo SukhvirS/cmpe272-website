b2f0d46ff2c939ff4832730049de5194265b2587
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-04-19 18:06:01 -0700

    testing

--- allUsers.php
+++ allUsers.php
@@ -31,71 +31,70 @@
     </head>
     <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
-            <h1>The Phone Company</h1>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-        
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="nav navbar-nav ml-auto">
-                <li class="nav-item">
-                <a class="nav-link active" href="index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="products.php">Products</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="news.php">News</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="about.php">About</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="users.php">Users</a>
-                </li>
-            </ul>
-            </div>
+        <h1>The Phone Company</h1>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+    
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="nav navbar-nav ml-auto">
+            <li class="nav-item">
+            <a class="nav-link active" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="products.php">Products</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="news.php">News</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="about.php">About</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="users.php">Users</a>
+            </li>
+        </ul>
+        </div>
 
-            <?php
+        <?php
+        session_start();
+
+        if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+            echo('
+            <form method="post"> 
+                <input type="submit" name="logout-button"
+                        class="btn btn-dark" value="Logout" /> 
+            </form> 
+                
+            ');
+        }
+
+        function logout(){
+            // Initialize the session
             session_start();
+            
+            // Unset all of the session variables
+            $_SESSION = array();
+            
+            // Destroy the session.
+            session_destroy();
+            
+            // Redirect to homepage
+            header("location: index.php");
+            exit;
+        }
 
-            if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-                echo('
-                <form method="post"> 
-                    <input type="submit" name="logout-button"
-                            class="btn btn-dark" value="Logout" /> 
-                </form> 
-                    
-                ');
-            }
+        if(array_key_exists('logout-button', $_POST)) { 
+            logout(); 
+        } 
 
-            function logout(){
-                // Initialize the session
-                session_start();
-                
-                // Unset all of the session variables
-                $_SESSION = array();
-                
-                // Destroy the session.
-                session_destroy();
-                
-                // Redirect to homepage
-                header("location: index.php");
-                exit;
-            }
-
-            if(array_key_exists('logout-button', $_POST)) { 
-                logout(); 
-            } 
-
-            ?>
-        </nav>
+        ?>
+    </nav>
         
-        <div>
+    <div>
         <table class="table">
           <thead>
             <tr>
-              <th scope="col">#</th>
               <th scope="col">First</th>
               <th scope="col">Last</th>
               <th scope="col">Address</th>
@@ -107,6 +106,7 @@
           <tbody>
             <?php
                 $ar1 = explode("\n", $result);
+                echo("----".$ar1);
                 $ar2 = Array();
                 foreach($ar1 as $el1){
                     echo("<tr>");
