b75c8b1ee67b7828a419940de8808071e3c76678
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-04-20 21:16:18 -0700

    testing

--- users.php
+++ users.php
@@ -109,7 +109,7 @@
           ?>
       </nav>
 
-      <div >
+      <div style="margin: 10px 10px 0 10px">
         <!-- <h2 style="float: left; margin: 0; padding: 0 0 0 6px">Users (of The Phone Company)</h2>
         &nbsp;&nbsp;-&nbsp;&nbsp;
         <a href="allUsers.php">All users</a> -->
