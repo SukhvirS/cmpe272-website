12ffa180e7dce8ad61a1953734473340a5ad75f7
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-04-21 19:47:17 -0700

    testing

--- allUsers.php
+++ allUsers.php
@@ -164,12 +164,12 @@
 
                 $ar2 = explode("|", $masonUsers);
                 $ar2 = array_splice($ar2, 0, count($ar2)-2);
-                echo("0".$ar[0].'-');
-                echo("1".$ar[1].'-');
-                echo("2".$ar[2].'-');
-                echo("3".$ar[3].'-');
-                echo("4".$ar[4].'-');
-                echo("5".$ar[5].'-');
+                echo("0".$ar2[0].'-');
+                echo("1".$ar2[1].'-');
+                echo("2".$ar2[2].'-');
+                echo("3".$ar2[3].'-');
+                echo("4".$ar2[4].'-');
+                echo("5".$ar2[5].'-');
                 for($i = 0; $i < count($ar2); $i+= 6){
                     if($i == 0){
                         echo("<tr>");
