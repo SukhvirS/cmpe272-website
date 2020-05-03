55042294320847be0b2a0cbfb1d2c13e07357248
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-05-03 16:28:33 -0700

    testing

---
+++ productApi.php
@@ -0,0 +1,15 @@
+<?php
+    $currentUrl = 'https://';
+    $currentUrl .= $_SERVER['HTTP_HOST'];
+    $currentUrl .= $_SERVER['REQUEST_URI'];
+    $indexInURL = strpos($currentUrl, 'index') + 6;
+    $index = substr($currentUrl, $indexInURL);
+    $index = intval($index);
+
+    $sql = "SELECT * FROM products WHERE productID = '$index'";
+    $result = mysqli_query($link, $sql);
+    $row = mysqli_fetch_assoc($result);
+
+    echo($row['name']);
+
+?>
