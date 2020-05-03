aac55df3443bd0b35090a9c236ecc5a405206faa
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-05-03 16:42:19 -0700

    testing

--- productApi.php
+++ productApi.php
@@ -3,9 +3,13 @@
     $currentUrl .= $_SERVER['HTTP_HOST'];
     $currentUrl .= $_SERVER['REQUEST_URI'];
     echo('url:'.$currentUrl);
+    echo("<br>");
     
     $indexInURL = strpos($currentUrl, 'id');
     echo("index of id:".$indexInURL);
+    echo("<br>");
+
+    echo(substr($currentUrl, $indexInURL));
 
     // $index = substr($currentUrl, $indexInURL);
     // $index = intval($index);
