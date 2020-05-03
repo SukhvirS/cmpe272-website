6cede476c6c289ecd1657192dd7503d3cb83ba6c
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-05-03 16:46:46 -0700

    testing

--- productApi.php
+++ productApi.php
@@ -1,15 +1,27 @@
 <?php
+
+    require_once 'config.php';
+
     $currentUrl = 'https://';
     $currentUrl .= $_SERVER['HTTP_HOST'];
     $currentUrl .= $_SERVER['REQUEST_URI'];
     echo('url:'.$currentUrl);
     echo("<br>");
     
-    $indexInURL = strpos($currentUrl, 'id');
-    echo("index of id:".$indexInURL);
+    $idIndex = strpos($currentUrl, 'id');
+    echo("index of id:".$idIndex);
     echo("<br>");
 
-    echo(substr($currentUrl, ($indexInURL+3)));
+    echo(substr($currentUrl, ($idIndex+3)));
+    echo("<br>");
+
+    $productID = intval(substr($currentUrl, ($idIndex+3)));
+
+    $sql = "SELECT * FROM products WHERE productID = '$productID'";
+    $result = mysqli_query($link, $sql);
+    $row = mysqli_fetch_assoc($result);
+
+    echo($row['name']);
 
     // $index = substr($currentUrl, $indexInURL);
     // $index = intval($index);
