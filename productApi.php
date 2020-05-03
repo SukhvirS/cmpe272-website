7b89018e4b77a51438d1b20b816d669764a34200
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-05-03 16:49:14 -0700

    testing

--- productApi.php
+++ productApi.php
@@ -5,15 +5,15 @@
     $currentUrl = 'https://';
     $currentUrl .= $_SERVER['HTTP_HOST'];
     $currentUrl .= $_SERVER['REQUEST_URI'];
-    echo('url:'.$currentUrl);
-    echo("<br>");
+    // echo('url:'.$currentUrl);
+    // echo("<br>");
     
     $idIndex = strpos($currentUrl, 'id');
-    echo("index of id:".$idIndex);
-    echo("<br>");
+    // echo("index of id:".$idIndex);
+    // echo("<br>");
 
-    echo(substr($currentUrl, ($idIndex+3)));
-    echo("<br>");
+    // echo(substr($currentUrl, ($idIndex+3)));
+    // echo("<br>");
 
     $productID = intval(substr($currentUrl, ($idIndex+3)));
 
@@ -22,6 +22,14 @@
     $row = mysqli_fetch_assoc($result);
 
     echo($row['name']);
+    echo("<br>");
+    echo($row['description']);
+    echo("<br>");
+    echo($row['price']);
+    echo("<br>");
+    echo("each");
+    echo("<br>");
+    echo($row['img1Url']);
 
     // $index = substr($currentUrl, $indexInURL);
     // $index = intval($index);
